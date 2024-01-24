87dc0b16f37215ac5fc60524731bf7a0c8f8be0f
Volodymyr Moiseichenko <volodymyr@getdentalray.com>
2024-01-24 14:08:32 +0200

    Add project files.

---
+++ MessagePack/Message.cs
@@ -0,0 +1,11 @@
+﻿using MessagePack;
+
+namespace WebSocketServer.MessagePack
+{
+    [MessagePackObject]
+    public sealed class Message
+    {
+        [Key(0)]
+        public string Content { get; set; }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,48 @@
+﻿using MessagePack;
+using System.Net;
+using System.Net.WebSockets;
+using WebSocketServer.MessagePack;
+
+namespace WebSocketServer
+{
+    public class Program
+    {
+        static async Task Main(string[] args)
+        {
+            HttpListener listener = new HttpListener();
+            listener.Prefixes.Add("http://*:5000/ws/");
+            listener.Start();
+
+            await Console.Out.WriteLineAsync("Listening...");
+
+            while (true)
+            {
+                HttpListenerContext context = await listener.GetContextAsync();
+                if (context.Request.IsWebSocketRequest)
+                {
+                    HttpListenerWebSocketContext wsContext = await context.AcceptWebSocketAsync(null);
+                    WebSocket webSocket = wsContext.WebSocket;
+
+                    //Обробка повідомлення WebSocket
+                    await SendWebSocketMessage(webSocket, "Hello World");
+
+                    await webSocket.CloseAsync(WebSocketCloseStatus.NormalClosure, String.Empty, CancellationToken.None);
+                }
+                else
+                {
+                    context.Response.StatusCode = 400;
+                    context.Response.Close();
+                }
+            }
+        }
+
+        static async Task SendWebSocketMessage(WebSocket webSocket, string messageContent)
+        {
+            var message = new Message { Content = messageContent };
+            byte[] messageBytes = MessagePackSerializer.Serialize(message);
+
+            await webSocket.SendAsync(new ArraySegment<byte>(messageBytes), WebSocketMessageType.Binary, true, CancellationToken.None);
+            Console.WriteLine("Message sent: " +  messageContent); 
+        }
+    }
+}
